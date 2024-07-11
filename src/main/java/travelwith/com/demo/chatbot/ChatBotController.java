3926c1f51ea3231a81b4f9ff564d27090909c8e4
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-07-11 21:08:50 +0900

    Feat: 추가. 타입 문제로 쿠키 관련 주석 처리
    ai를 혼내거나 답변이 이상하다고 지적하면 503 에러가 뜸.

--- src/main/java/travelwith/com/demo/chatbot/ChatBotController.java
+++ src/main/java/travelwith/com/demo/chatbot/ChatBotController.java
@@ -34,31 +34,29 @@ public class ChatBotController {
     @GetMapping("/chatbot")
     public String chattingPage(@CookieValue(value = "ask_token", required = false) Cookie cookie, HttpServletResponse response) {
 
-        if (cookie == null) {// 사용자에게 쿠키가 없으면 쿠키 새롭게 발급
+//        if (cookie == null) {// 사용자에게 쿠키가 없으면 쿠키 새롭게 발급
             UUID uuid = UUID.randomUUID();
             chatBotService.setTokenCookie(response, uuid);
-            chatBotService.saveNewCookie(uuid.toString());
+//            chatBotService.saveNewCookie(uuid.toString());
             chatBotService.saveConversation(uuid.toString(), new ConversationLog(uuid.toString(), LocalDateTime.now().toString(), "대화 시작", "대화를 시작합니다"));
-
-        } else { // 사용자가 쿠키 보유하고 있을시 쿠키 유효성 검사
-            String cookieValue = cookie.getValue();
-            try{
-                boolean cookieValid = chatBotService.validateCookie(cookie, chatBotService.getAskCount(cookieValue));
-                if (!cookieValid) { // 쿠키가 이상하면 0 반환. 정상이면 1반환
-                    return "0";
-                }
-            }catch (NullPointerException e){
-                return "0";
-            }
-
-
-        }
+//
+//        } else { // 사용자가 쿠키 보유하고 있을시 쿠키 유효성 검사
+//            String cookieValue = cookie.getValue();
+//            try{
+//                boolean cookieValid = chatBotService.validateCookie(cookie, chatBotService.getAskCount(cookieValue));
+//                if (!cookieValid) { // 쿠키가 이상하면 0 반환. 정상이면 1반환
+//                    return "0";
+//                }
+//            }catch (NullPointerException e){
+//                return "0";
+//            }
+//        }
         return "1";
     }
 
     @GetMapping("/chatbot/chatting")
     public DeferredResult<String> chatWithBedrock(@RequestParam("prompt") String prompt,
-                                                  @CookieValue("ask_token") Cookie token) {
+                                                  @CookieValue(value = "ask_token", required = false) Cookie token) {
         // 주의점
         // 1. 자바에서 외부 api 호출은 기본적으로 동기식. 따로 설정해줘야 함.
         // 2. 비동기로 구현했으나, prompt가 같은 요청은 동기식으로 작동함. 이유는 모르겠음.
@@ -66,21 +64,21 @@ public class ChatBotController {
         String cookieValue = token.getValue();
         log.info("token value = {}", cookieValue);
         DeferredResult<String> deferredResult = new DeferredResult<>();
-
-        int askCount = 0;
-
-        try {// askTokenStorage 에 저장된 쿠키값인지 확인
-            askCount = chatBotService.getAskCount(cookieValue);
-        } catch (NullPointerException e) {
-            log.info("유효하지 않은 쿠키로 채팅 시도");
-            deferredResult.setResult("유효하지 않은 접근입니다.");
-        }
-
-        String countCheck = checkAskCount(askCount);
-        if (countCheck != null) { // 질문 횟수 체크
-            deferredResult.setResult(countCheck);
-            return deferredResult;
-        }
+//
+//        int askCount = 0;
+//
+//        try {// askTokenStorage 에 저장된 쿠키값인지 확인
+//            askCount = chatBotService.getAskCount(cookieValue);
+//        } catch (NullPointerException e) {
+//            log.info("유효하지 않은 쿠키로 채팅 시도");
+//            deferredResult.setResult("유효하지 않은 접근입니다.");
+//        }
+//
+//        String countCheck = checkAskCount(askCount);
+//        if (countCheck != null) { // 질문 횟수 체크
+//            deferredResult.setResult(countCheck);
+//            return deferredResult;
+//        }
 
         if (prompt.isEmpty()) { // 질문 내용 공백 체크
             deferredResult.setResult(NO_ANSWER);
@@ -117,7 +115,7 @@ public class ChatBotController {
             }
         });
 
-        chatBotService.increaseAskCount(cookieValue);
+//        chatBotService.increaseAskCount(cookieValue);
         return deferredResult;
     }
 
--- src/main/java/travelwith/com/demo/chatbot/ChatBotService.java
+++ src/main/java/travelwith/com/demo/chatbot/ChatBotService.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @RequiredArgsConstructor
 public class ChatBotService {
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, ConversationLog> redisTemplate;
     private final Gson gson;
 
     private String apiGatewayUrl = "https://wrrvutyink.execute-api.us-west-2.amazonaws.com/dev/prompt";
@@ -64,14 +64,14 @@ public class ChatBotService {
         }
     }
 
-    public void saveNewCookie(String cookie){
-        redisTemplate.opsForValue().setIfAbsent(cookie, 0);
-        redisTemplate.expire(cookie, 12, TimeUnit.HOURS);
-    }
+//    public void saveNewCookie(String cookie){
+//        redisTemplate.opsForValue().setIfAbsent(cookie, 0);
+//        redisTemplate.expire(cookie, 12, TimeUnit.HOURS);
+//    }
 
-    public int getAskCount(String coookie){
-        return (int) redisTemplate.opsForValue().get(coookie);
-    }
+//    public int getAskCount(String coookie){
+//        return (int) redisTemplate.opsForValue().get(coookie);
+//    }
 
     public void increaseAskCount(String cookie){
         redisTemplate.opsForValue().increment(cookie);
@@ -79,14 +79,14 @@ public class ChatBotService {
 
 
     public void saveConversation(String key, ConversationLog conversation) {
-        ListOperations<String, Object> listOps = redisTemplate.opsForList();
+        ListOperations<String, ConversationLog> listOps = redisTemplate.opsForList();
         listOps.leftPush(key, conversation);  // 최신 대화를 왼쪽으로 추가
         listOps.trim(key, 0, 9);  // 리스트의 길이를 최대 10개로 유지
         redisTemplate.expire(key, 12, TimeUnit.HOURS);
     }
 
     public String getConversations(String key) {
-        ListOperations<String, Object> listOps = redisTemplate.opsForList();
+        ListOperations<String, ConversationLog> listOps = redisTemplate.opsForList();
         return gson.toJson(listOps.range(key, 0, -1));
     }
 
