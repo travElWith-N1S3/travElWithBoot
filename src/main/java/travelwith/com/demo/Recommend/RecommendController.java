bc80b43a905247dc35e0421e3033712851a8de84
sinalol15 <pnsesh01@naver.com>
2024-07-16 14:24:06 +0900

    Docs: 추가. 10페이지씩 페이징 및 검색 기능 추가

--- src/main/java/travelwith/com/demo/Recommend/RecommendController.java
+++ src/main/java/travelwith/com/demo/Recommend/RecommendController.java
@@ -1,40 +1,41 @@
 package travelwith.com.demo.Recommend;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 public class RecommendController {
-    private final SpotService spotService;
+	private final SpotService spotService;
 
-    @GetMapping("/v1/popular-destination")
-    public List<String> findPopularSpot(){
-        return spotService.findPopularSpot();
+	@GetMapping("/v1/popular-destination")
+	public List<String> findPopularSpot() {
+		return spotService.findPopularSpot();
+	}
+
+	@GetMapping("/v1/destination/info")
+	public SpotDto getRecommendSpot(@RequestParam("id") int id) {
+		return spotService.findById(id);
+	}
+
+	@GetMapping("/v1/destinationList")
+    public Page<SpotDto> getAllSpot(@RequestParam(value = "query", required = false) String query,
+                                    @RequestParam("page") int page,
+                                    @RequestParam("size") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return spotService.findAllPage(query, pageable);
     }
 
-    @GetMapping("/v1/destination/info")
-    public SpotDto getRecommendSpot(@RequestParam("id") int id) {
-        return spotService.findById(id);
-    }
-
-    @GetMapping("/v1/destinationList")
-    public Page<SpotDto> getAllSpot(@PageableDefault(page = 1) Pageable pageable) {
-        return spotService.findAllPage(pageable);
-    }
-
-    @GetMapping("/v1/top-tour-spot")
-    public List<SpotDto> get3spot(){
-        return spotService.findTop3Spot();
-    }
+	@GetMapping("/v1/top-tour-spot")
+	public List<SpotDto> get3spot() {
+		return spotService.findTop3Spot();
+	}
 }
